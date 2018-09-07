415aa50bff1b39b9ef3e5012df1d4681d368d1a1
Myke Phillips <info@thefraternity.co.uk>
2018-09-07 22:00:24 +0100

    Added number formatter for currency.

--- Widow Shopper/View/CurrencyTextfield.swift
+++ Widow Shopper/View/CurrencyTextfield.swift
@@ -11,6 +11,22 @@ import UIKit
 @IBDesignable
 class CurrencyTextfield: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 0.7959118151)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+
     override func prepareForInterfaceBuilder() {
         customizeView()
 
@@ -26,6 +42,7 @@ class CurrencyTextfield: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2507491438)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
 
         if let placeHolder = placeholder {
             let place = NSAttributedString(string: placeHolder, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
